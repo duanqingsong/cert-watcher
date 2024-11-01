86ee104b41284a6fcf94b85eb89fafae2eaeccae
duanqingsong <30310751+duanqingsong@users.noreply.github.com>
2024-11-01 23:11:44 +0800

    修改配置

--- next.config.js
+++ next.config.js
@@ -3,13 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {protocol: 'https',hostname: 'images.pexels.com',port: '',pathname: '/photos/**',},
-      {protocol: 'http',hostname: 'localhost',port: '3001',pathname: '/**',},
-      {protocol: 'http',hostname: 'localhost',port: '8080',pathname: '/**',},
-      {protocol: 'http',hostname: '127.0.0.1',port: '8080',pathname: '/**',},
-      {protocol: 'http',hostname: 'expo.site.img.utour.xin',port: '',pathname: '/**',},
-      {protocol: 'https',hostname: 'res.bracexpo.com',port: '',pathname: '/**',},
     ],
-    domains: ['res.bracexpo.com','localhost:8080'],
+    domains: ['certs.utour.xin','localhost:3009'],
     deviceSizes: [640,750,828,1080,1200,1920,2048,3840],
     imageSizes: [16,32,48,64,96,128,256,384],
     minimumCacheTTL: 60,
