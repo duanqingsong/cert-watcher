ffc7139b8c7acb0344fca0c65d1fedc809adc542
duanqingsong <30310751+duanqingsong@users.noreply.github.com>
2025-10-25 18:52:59 +0800

    增加自检发送通知的逻辑

--- cron-server.js
+++ cron-server.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const dotenv = require('dotenv');
 const path = require('path');
-const { performCertificateCheck,notifyCertificateExpiry } = require('./lib/certCheck');
+const { performCertificateCheck,notifyCertificateExpiry,send2Wechat } = require('./lib/certCheck');
 
 // 从命令行参数或环境变量获取 .env 文件路径
 const envFileArg = process.argv.find(arg => arg.startsWith('--env=')) || '--env=';
@@ -48,6 +48,13 @@ if(!notifySchedule){
 }
 if(notifySchedule){
   cron.schedule(notifySchedule, async () => {
+    // 增加自检通知,防止长久没有通知,确保知道服务是正常运行的
+    // 增加每周1,3,5 发送一次通知.
+    // 因为此任务每天执行一次,所以这里只需要判断当天是否是1,3,5,发送一次即可
+    const dayOfWeek = new Date().getDay();
+    if (dayOfWeek === 1 || dayOfWeek === 3 || dayOfWeek === 5) {
+      await send2Wechat('证书到期检查定时任务正常运行中...','13926576007');
+    }
     console.log('开始->定时证书到期通知任务:', new Date().toLocaleString());
     try {
       await notifyCertificateExpiry();
